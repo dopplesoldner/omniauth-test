233d84bf450f49ea792e1ade3738eec3be4bc68f
Mohammad Rashid <dopplesoldner@gmail.com>
2013-04-28 13:35:01 +0100

    Facebook authentication using omniauth-facebook

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmniauthTutorial::Application.config.secret_key_base = 'eb56a57aeff802e0faf1fcc47698c42ea922fb4bbbfe83f081fbdd1ae1c7d998cb75f75be88dbd28e0530e7464605dd15cec137bef83f84105db707231592149'
